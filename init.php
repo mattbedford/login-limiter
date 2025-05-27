334c78ba87789d6c1699919d53401ad327562c20
mattbedford <matt.bedford#gmail.com>
2025-05-27 21:51:49 +0200

    Update version number

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Login Attempt Limiter
 Description: Limits login attempts with ban/whitelist, REST API management, and admin settings/log viewer.
-Version: 1.4
+Version: 1.5
 Author: Matt Bedford
 Author uri: https://mattbedford.com
 Text domain: lal;
