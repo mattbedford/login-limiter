d8018c32cbb08a40803075d00f6e91646db93061
mattbedford <matt.bedford#gmail.com>
2025-08-11 18:08:20 +0200

    Add missing includes

--- init.php
+++ init.php
@@ -22,15 +22,10 @@ require_once __DIR__ . '/src/LoginLimiter.php';
 require_once __DIR__ . '/src/SettingsPage.php';
 require_once __DIR__ . '/src/RestAPI.php';
 require_once __DIR__ . '/src/StopUserEnumeration.php';
+require_once __DIR__ . '/src/Security/Turnstile.php';
+require_once __DIR__ . '/src/Security/TurnstileOptions.php';
 require_once __DIR__ . '/src/TurnstileHandler.php';
 
-
-
-// On activate, trigger table creation for ip addresses
-register_activation_hook( __FILE__, static function() {
-    require_once plugin_dir_path( __FILE__ ) . '/src/DBHandler.php';
-    \LAL\src\DBHandler::create_table();
-} );
 
 
 add_action('plugins_loaded', function () {
